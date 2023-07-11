835f25dbf36c810002af38dbef90fb5245ea1aa3
devendra <dev.kumar89.dp@gmail.com>
2023-07-11 12:58:53 +0530

    Update Counter.js
    explanation useReducer function

--- usereducer/src/Counter.js
+++ usereducer/src/Counter.js
@@ -26,6 +26,26 @@ const Counter = () => {
         <button onClick={() => dispatch({type: 'INC'})}>Inc</button>
         <span>{state}</span>
         <button onClick={() => dispatch({type: 'DEC'})}>Dec</button>
+
+        // The useReducer(reducer, initialState) hook accepts 2 arguments: the reducer function and the initial state. The hook then returns an array of 2 items: the current state and the dispatch function.
+        
+        // The reducer is a pure function that accepts 2 parameters: the current state and an action object. Depending on the action object, the reducer function must update the state in an immutable manner, and return the new state.
+        
+        // function reducer(state, action) {
+        //   let newState;
+        //   switch (action.type) {
+        //     case 'increase':
+        //       newState = { counter: state.counter + 1 };
+        //       break;
+        //     case 'decrease':
+        //       newState = { counter: state.counter - 1 };
+        //       break;
+        //     default:
+        //       throw new Error();
+        //   }
+        //   return newState;
+        // }
+
     </>
   )
 }
