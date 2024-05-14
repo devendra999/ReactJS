92d86e8a8134727099d5856c972d34b3e60d4ca0
devendra <dev.kumar89.dp@gmail.com>
2024-05-14 19:04:40 +0530

    Update UploadImagePreview.js
    Update UploadImagePreview.js

--- demo/uploadImagePreview/UploadImagePreview.js
+++ demo/uploadImagePreview/UploadImagePreview.js
@@ -1,20 +1,39 @@
-import React, { useRef, useState } from 'react'
+'use client'
+import React, { useState } from 'react';
 
 const UploadImagePreview = () => {
-    const [file, setFile] = useState();
+    const [file, setFile] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
-        setFile(URL.createObjectURL(e.target.files[0]));
+        const selectedFile = e.target.files[0];
+        if (selectedFile) {
+            const fileExtension = selectedFile.name.split('.').pop().toLowerCase();
+            if (fileExtension === 'jpg' || fileExtension === 'png') {
+                if (selectedFile.size <= 300 * 1024) { // Convert KB to bytes
+                    setFile(URL.createObjectURL(selectedFile));
+                    setErrorMessage('');
+                } else {
+                    setFile(null);
+                    setErrorMessage('File size exceeds 300 KB.');
+                }
+            } else {
+                setFile(null);
+                setErrorMessage('Please select a .jpg or .png file.');
+            }
+        }
     }
 
-  return (
-    <div className="upload">
-        <div className="container">
-              <h2>Add Image:</h2>
-              <input type="file" onChange={handleChange} />
-              <img src={file} style={{ width: '100px', height: '100px', border: '2px solid #000', borderRadius: '100%' }} />
+    return (
+        <div className="upload">
+            <div className="container">
+                <h2>Add Image:</h2>
+                <input type="file" accept=".jpg,.png" onChange={handleChange} />
+                {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                {file && <img src={file} style={{ width: '100px', height: '100px', border: '2px solid #000', borderRadius: '100%' }} alt="Preview" />}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default UploadImagePreview
+export default UploadImagePreview;
